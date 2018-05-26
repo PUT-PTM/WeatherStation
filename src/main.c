770177eaf7e8390ddde37d104a7f11e46538ae52
jlorc <37503155+jlorc@users.noreply.github.com>
2018-05-27 00:36:48 +0200

    unused function declarations deleted

--- src/main.c
+++ src/main.c
@@ -11,19 +11,6 @@ double tempValue;
 double humValue;
 char buffer[10];
 
-void send_char(char c)
-{
-	while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
-	USART_SendData(USART2, c);
-}
-
-int __io_putchar(int c)
-{
-	if (c=='\n')
-		send_char('\r');
-	send_char(c);
-	return c;
-}
 
 void computeTemperatureAndHumidity() {
 	static uint16_t temp16;
